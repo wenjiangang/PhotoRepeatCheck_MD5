089539013deb7175c5bfab1e5042a06213321eaf
zhejiang <wenjg11@chinaunicom.cn>
2018-10-25 10:44:15 +0800

    20181025上午10点修改了src/md5calculate/filesArray.java，方法通过递归返回目录下所有的文件。

--- src/md5calculate/filesArray.java
+++ src/md5calculate/filesArray.java
@@ -2,6 +2,12 @@ package md5calculate;
 import java.io.File;
 import java.util.ArrayList;
 
+/*
+ * getFiles函数返回文件的目录，参数path为文件目录，返回值类型为ArrayList
+ * 
+ * 
+ */
+
 public class filesArray {
 	public static ArrayList<String> getFiles(String path) {
 	    ArrayList<String> files = new ArrayList<String>();
@@ -10,14 +16,23 @@ public class filesArray {
 
 	    for (int i = 0; i < tempList.length; i++) {
 	        if (tempList[i].isFile()) {
-//	              System.out.println("文     件：" + tempList[i]);
+	              System.out.println("文     件：" + tempList[i]);
 	            files.add(tempList[i].toString());
 	        }
-//	        if (tempList[i].isDirectory()) {
-//	              System.out.println("文件夹：" + tempList[i]);
-//	        }
+	        if (tempList[i].isDirectory()) {
+	              System.out.println("文件夹：" + tempList[i]); 
+	              files.addAll(getFiles(tempList[i].toString()));
+	        }
 	    }
 	    return files;
+	}
+	
+	public static void main(String[] args){
+		String pathTest="C:/Users/Unicom/Desktop/new"; //测试目录下文件是否都被读取到了
+		ArrayList<String> fileLists = getFiles(pathTest);
+		System.out.println(fileLists+"\r\n"+fileLists.size());
+		
+		
 	}
 
 }

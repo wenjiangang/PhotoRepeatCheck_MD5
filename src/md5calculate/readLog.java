99e4ce78933c71e631da66d0b6fb5a255056db6e
wenjiangang <lewin.cn@gmail.com>
2018-10-28 20:01:15 +0800

    20181028修改了readLog类的说明

--- src/md5calculate/readLog.java
+++ src/md5calculate/readLog.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 根据1-8楼的建议，优化了代码
+ * 该类用于读取和写入文件内容
  */
 public class readLog {
 
@@ -25,12 +25,10 @@ public class readLog {
 		// 绝对路径或相对路径都可以，写入文件时演示相对路径,读取以上路径的input.txt文件
 		// 防止文件建立或读取失败，用catch捕捉错误并打印，也可以throw;
 		// 不关闭文件会导致资源的泄露，读写文件都同理
-		// Java7的try-with-resources可以优雅关闭文件，异常时自动关闭文件；详细解读https://stackoverflow.com/a/12665271
 		HashMap<String, String> list = new HashMap<>();
 		try (FileReader reader = new FileReader(pathname); BufferedReader br = new BufferedReader(reader) // 建立一个对象，它把文件内容转成计算机能读懂的语言
 		) {
 			String line;
-			// 网友推荐更加简洁的写法
 			String[] aa = null;
 			while ((line = br.readLine()) != null) {
 				// 一次读入一行数据
